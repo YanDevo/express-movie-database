27faf4199e951216fa94d4d6f5323a0cdd3917d9
Wade <wadeya@ad.vzwcorp.com>
2021-12-10 10:43:06 -0600

    make model associations

---
+++ index.js
@@ -0,0 +1,17 @@
+const {sequelize, DataTypes, Model} = require('./db')
+
+// import your models
+const {Movie} = require('../models/Movie')
+const {Cast} = require('../models/Cast')
+const {Crew} = require('../models/Crew')
+
+
+// associate
+Crew.belongsTo(Movie)
+Cast.belongsTo(Movie)
+
+Movie.hasMany(Cast)
+Movie.hasMany(Crew)
+
+//export
+module.exports = {Movie, Cast, Crew, sequelize}
